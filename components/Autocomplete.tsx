727d2e671b6ff95aa2d02fe65be1970655bc0287
ikyro <ikyroch@gmail.com>
2022-10-13 04:36:54 -0300

    feat: add components

---
+++ components/Autocomplete.tsx
@@ -0,0 +1,44 @@
+import { useAutocomplete } from 'hooks/useAutocomplete'
+import { getPokemon, Pokemon, Records } from 'lib/pokemon'
+import { useState } from 'react'
+import { PokemonInfo } from 'components/PokemonInfo'
+
+export const Autocomplete = ({
+  autocomplete,
+  random,
+}: {
+  autocomplete: Records[]
+  random: Pokemon
+}) => {
+  const { query, register } = useAutocomplete(autocomplete)
+  const [selected, setSelected] = useState(random)
+
+  return (
+    <>
+      <input
+        type='text'
+        className='w-60 rounded-sm bg-[#222224] p-2 text-center text-white outline-none lg:w-72 lg:p-3'
+        placeholder='Search Pokemon'
+        autoComplete='off'
+        {...register('autocomplete')}
+      />
+      <div className='grid grid-rows-2 gap-2 lg:grid-cols-2 lg:grid-rows-none'>
+        <ul className='scroll mt-2 flex max-h-72 w-60 flex-col gap-2 overflow-auto bg-[#222224] p-2 text-white lg:h-72 lg:w-72 lg:p-3'>
+          {query.map(({ name, url }) => (
+            <li
+              className='w-full rounded-sm bg-[#ee1515] py-1 text-center text-sm'
+              onClick={async () => {
+                const pokemon = await getPokemon(url)
+                setSelected(pokemon)
+              }}
+              key={name}
+            >
+              <button>{name}</button>
+            </li>
+          ))}
+        </ul>
+        <PokemonInfo selected={selected} />
+      </div>
+    </>
+  )
+}
---
+++ components/PokemonInfo.tsx
@@ -0,0 +1,48 @@
+import { motion } from 'framer-motion'
+import { Pokemon } from 'lib/pokemon'
+import Image from 'next/image'
+
+export const PokemonInfo = ({ selected }: { selected: Pokemon }) => {
+  return (
+    <div className='mt-2 h-72 w-60 bg-[#222224] p-2.5 text-center text-white'>
+      <h1 className='text-lg'>{selected.name}</h1>
+      <h2 className='text-xs'>{`#${selected.id}`}</h2>
+      <Image
+        src={
+          selected.sprites?.front_default ??
+          selected.sprites?.back_default ??
+          '/placeholder.png'
+        }
+        alt={`pokemon from image`}
+        width={100}
+        height={100}
+      />
+      <Image
+        src={
+          selected.sprites?.back_default ??
+          selected.sprites?.front_default ??
+          '/placeholder.png'
+        }
+        alt={`pokemon back image`}
+        width={100}
+        height={100}
+      />
+      <span className='text-ms'>width: {selected.weight}</span>
+      <span className='text-ms ml-4'>height: {selected.weight}</span>
+
+      <ul className='mt-4 flex flex-wrap items-center justify-center gap-2'>
+        {selected.types.map(({ type: { name } }) => (
+          <motion.li
+            className='rounded-sm bg-[#ee1515] py-1 px-2 text-center text-sm'
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0 }}
+            key={name}
+          >
+            {name}
+          </motion.li>
+        ))}
+      </ul>
+    </div>
+  )
+}
